835a8edd31470f4a645e276278e2ce6d829c3a95
Hestice <custillano@gmail.com>
2023-07-15 18:49:05 +0800

    Navbar and carousel

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var slides = document.querySelectorAll(".carousel-slide");
+    var currentSlide = 0;
+  
+    function showSlide(n) {
+      if (n >= slides.length) {
+        currentSlide = 0;
+      } else if (n < 0) {
+        currentSlide = slides.length - 1;
+      }
+  
+      for (var i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+      }
+  
+      slides[currentSlide].classList.add("active");
+    }
+  
+    function nextSlide() {
+      currentSlide++;
+      showSlide(currentSlide);
+    }
+  
+    setInterval(nextSlide, 3000); // Change slide every 3 seconds
+  
+    showSlide(currentSlide);
+  });
+  
