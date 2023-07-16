782a798fe504089fa3cf2b4252a16845ce752d57
Hestice <custillano@gmail.com>
2023-07-16 18:19:37 +0800

    carousel indicator visuals and transition animation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var slides = document.querySelectorAll(".carousel-slide");
+    var dots = document.querySelectorAll(".dot");
     var currentSlide = 0;
   
     function showSlide(n) {
@@ -14,12 +15,28 @@ document.addEventListener("DOMContentLoaded", function() {
       }
   
       slides[currentSlide].classList.add("active");
+
+      for (var i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+      }
+  
+      dots[currentSlide].classList.add("active");
     }
   
-    function nextSlide() {
+    window.nextSlide = function() {
       currentSlide++;
       showSlide(currentSlide);
-    }
+    };
+
+    window.prevSlide = function() {
+      currentSlide--;
+      showSlide(currentSlide);
+    };
+
+    window.currentSlide = function(n) {
+      currentSlide = n;
+      showSlide(currentSlide);
+    };
   
     setInterval(nextSlide, 5000); // Change slide every 3 seconds
   
