454ba11e7bb95a6c7871d6582037e74fc70fc924
Hestice <custillano@gmail.com>
2023-07-16 18:43:36 +0800

    debugging visuals

--- script.js
+++ script.js
@@ -43,3 +43,13 @@ document.addEventListener("DOMContentLoaded", function() {
     showSlide(currentSlide);
   });
   
+  // Add smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
